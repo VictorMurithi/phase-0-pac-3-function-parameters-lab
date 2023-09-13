93a592c86c895ae2c7accce29299fb847931c962
victor <victor.murithi@student.moringaschool.com>
2023-09-13 20:01:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function introduction(name){
-    return `Hi, my name is ${name}.`;
+    return  `Hi, my name is ${name}.`;
 }
-function introductionWithLanguage(name,language){
+function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-function introductionWithLanguageOptional(name,language="Javascript"){
+function introductionWithLanguageOptional(name, language="JavaScript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
